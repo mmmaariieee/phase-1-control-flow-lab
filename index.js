e1b37f251f628f7c5f8a60fac30e3b3236957a49
Mariia <m.goncharenkoo@icloud.com>
2022-09-18 19:55:47 -0400

    commit tree 7415040bc1e35424265d51dcd66e393d5612db3d

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+
+  const reply1 = "This one is on me!";
+  const reply2 = "That will be twenty bucks.";
+  const reply3 = "I will gladly take your thirty bucks.";
+  const reply4 = "No can do.";
+
+  if (someNumber <= 400){
+    return reply1;
+  }
+  else if (someNumber > 400 && someNumber <= 2000){
+    return reply2;
+  }
+  else if (someNumber > 2000 && someNumber <= 2500){
+    return reply3;
+  }
+  else if(someNumber > 2500){
+   return reply4;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch (tip) {
+    case 'generous':
+      return'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return"Bye.";
+      break;
+  }
+
 }
